7db91d9eb9d3b91f841031372d52d71b218c2469
Renatuscape <silarcta@gmail.com>
2024-11-04 15:35:41 +0100

    Sets up DbContext for building the database

--- MovieBackendPresentation/Data/MovieBackendDbContext.cs
+++ MovieBackendPresentation/Data/MovieBackendDbContext.cs
@@ -3,5 +3,11 @@ using MovieBackendPresentation.Data.Models;
 
 namespace MovieBackendPresentation.Data
 {
+    public class MovieBackendDbContext : DbContext
+    {
+        public MovieBackendDbContext(DbContextOptions<MovieBackendDbContext> options) : base(options) { }
 
+        public DbSet<User> User { get; set; } = default!;
+        public DbSet<Movie> Movie { get; set; } = default!;
+    }
 }
