8a512ff9dfd95ab45f58154816daece9c62e3d81
Renatuscape <silarcta@gmail.com>
2024-11-04 15:39:26 +0100

    Makes UserMovie nullable and initialises all string values
    Prevents warnings, and allows movie to be created without being added to someone's favourites.

--- MovieBackendPresentation/Data/Models/Movie.cs
+++ MovieBackendPresentation/Data/Models/Movie.cs
@@ -2,8 +2,8 @@ namespace MovieBackendPresentation.Data.Models;
 
 public class Movie {
 	public int Id { get; set; }
-	public string Title { get; set; }
-	public string Genre { get; set; }
-	public string ImgUrl { get; set; }
-	public ICollection<UserMovie> UserMovie { get; set; }
+	public string Title { get; set; } = string.Empty;
+	public string Genre { get; set; } = string.Empty;
+    public string ImgUrl { get; set; } = string.Empty;
+    public ICollection<UserMovie>? UserMovie { get; set; }
 }
